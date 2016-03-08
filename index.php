96e8179df83bd64f61c7c32175b097e259c86f34
trollepierre <trolle.pierre@gmail.com>
2016-03-08 15:44:30 +0100

    Amélioration Propreté du Code

--- index.php
+++ index.php
@@ -36,38 +36,45 @@ sec_session_start();
 	
 	<script>
 	$(function(){
-		afficheConversation();
+		showConversation();
 		
 		$('#sent').click(function(){
 			var message = $('#btn-input').val();
 			alert("Votre message a bien été envoyé !");
 			$.post('chat.php',{
-				'nom':"<?php echo $_SESSION['username']; ?>",
+				'nom':getSessionUsername(),
 				'message':message
 			},
 			function(){
-				afficheConversation();
+				showConversation();
 				$('#btn-input').val('');
 				$('#message').focus();
-			});
-		});
+			},
+			function(){
+				alert("Post non envoyé !");
+			}
+			);
 
-		function afficheConversation(){
-			//Vider #conversation
+		});
+		function getSessionUsername(){
+			return "<?php echo $_SESSION['username']; ?>";
+		}
+
+		function showConversation(){
 			$("#conversation").empty();
 
 			$.getJSON('conversation.json', function(donnees){
 				$.each(donnees, function(key,val){
-					if (val.nom !=="<?php echo $_SESSION['username']; ?>") {
-						$('#conversation').append('<li class="left clearfix"><span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+val.nom +'</strong> <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span>' + val.when +'</small></div><p>' + val.message + '</p></div></li>');
-					}else{
+					if (val.nom ===getSessionUsername()) {
 						$('#conversation').append('<li class="right clearfix"><span class="chat-img pull-right"><img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><small class=" text-muted"><span class="glyphicon glyphicon-time"></span>'+val.when+'</small><strong class="pull-right primary-font">'+val.nom +'</strong></div><p>' + val.message + '</p></div></li>');
+					}else{
+						$('#conversation').append('<li class="left clearfix"><span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+val.nom +'</strong> <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span>' + val.when +'</small></div><p>' + val.message + '</p></div></li>');
 					}
 				});
 			});
 			$('#message').focus();
 		}
-		setInterval(afficheConversation, 40000);
+		setInterval(showConversation, 4000);
 	});
 	</script>
    <?php endif; ?>
